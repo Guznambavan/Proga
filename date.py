5a55d119f63b7013acc18878688235a1fdb7b4fe
Guznambavan <117712243+Guznambavan@users.noreply.github.com>
2024-09-21 15:51:31 +0700

    Add files via upload

---
+++ date.py
@@ -0,0 +1,8 @@
+import random
+
+def find_place (list_places):
+    index = randint(1, len(list_places))
+    return (list_places[index])
+
+
+list_date = ['ШреддерПицца', 'ХанБуз', 'АртПаб', 'Гуси', 'Камин', 'RedRabbit', 'Shurubor']
